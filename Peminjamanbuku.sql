58f1b69c08b725d4fcfae7b8bb2b2434089a17b4
muhammadhaerularul-wq <muhammadhaerularul@gmail.com>
2026-01-19 08:28:39 +0700

    Add files via upload

---
+++ Peminjamanbuku.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Jan 2026 pada 01.21
+-- Versi server: 10.4.6-MariaDB
+-- Versi PHP: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `Peminjamanbuku`
+--
+
+CREATE TABLE `Peminjamanbuku` (
+  `id_anggota` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `alamat` text NOT NULL,
+  `No telp` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `tanggal daftar` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
